50dbb0c5ec7ac22a08179f697d433903098f5e7d
miyazakiharuki <zm18493@josai.ac.jp>
2021-01-26 04:01:27 +0000

    0126 no.2

--- ex07-1.js
+++ ex07-1.js
@@ -6,13 +6,16 @@ function txtchange3() {
     document.getElementById('txt1').innerHTML= 'ザンネン';}
 function txtchange4() {
     document.getElementById('txt1').innerHTML= '正解！ 解説：推理小説→「す」入り小説';}
-    
+
 function txtchange5() {
-    document.getElementById('txt2').innerHTML= 'チガウヨ';}
+    document.getElementById('txt2').innerHTML= '日本　解説：「電気をつけろと日本語で言っているから';}
+
 function txtchange6() {
-    document.getElementById('txt2').innerHTML= 'ピンポーン！　解説：芯出る（死んでる）';}
+    document.getElementById('txt3').innerHTML= 'チガウヨ';}
 function txtchange7() {
-    document.getElementById('txt2').innerHTML= 'ちがうよ';}
+    document.getElementById('txt3').innerHTML= 'ピンポーン！　解説：芯出る（死んでる）';}
 function txtchange8() {
-    document.getElementById('txt2').innerHTML= '違うよ';}
+    document.getElementById('txt3').innerHTML= 'ちがうよ';}
+function txtchange9() {
+    document.getElementById('txt3').innerHTML= '違うよ';}
     
---
+++ ex07-2.js
@@ -0,0 +1,5 @@
+var who72 ='world';
+function sayhello72(){
+    alert('どうも,' + who72 +'だよ!');}
+function someone(x){
+    who72 = x;}
