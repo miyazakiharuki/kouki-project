fb2ec8cef966846b39726e72aa5e4b74ec736026
miyazakiharuki <zm18493@josai.ac.jp>
2021-02-09 05:22:46 +0000

    0209

--- ex07-5.js
+++ ex07-5.js
@@ -1,7 +1,7 @@
 function nazo2()
 {if (document.getElementById('answer2').value=='日本')
 {alert('あたりだワン!　解説：「電気をつけろ」と日本語で言っているから');}
-else{alert('はずれ！');}}
+else{alert('はずれだワン！');}}
 
 function nazo3()
 {if (document.getElementById('answer3').value=='3')
